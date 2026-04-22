c3d05d3ed10dfef15a95c9894445cde7c471725b
Mhmd7sanin <iihassanin4@gmail.com>
2026-04-22 07:39:50 +0200

    Add initial template for competitive programming solutions for codeforces

---
+++ templates/template.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define fast ios::sync_with_stdio(false); cin.tie(NULL);
+
+void solve()
+{
+    // your code
+}
+
+
+int main() {
+    fast;
+    solve();
+}
+
+
+
+/*
+Problem Name: 
+Link: 
+Rating: 
+Tags: 
+
+Time Complexity: O(1)
+*/
